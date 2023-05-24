d8fdcabf0b7841687698f5378960f73d5eb16448
Anna V <lianna2015new@yandex.ru>
2023-05-24 17:14:27 +1000

    cranes

--- test -4.py
+++ test -4.py
@@ -1,6 +1,11 @@
 total = int (input ('введи общее число журавликов '))
-Sergei = int (total/3)//2
-Petr =  int (total/3)//2
-Kate = int (Sergei + Petr ) * 2
-print (f'{total} {Sergei, Kate, Petr}')
-print (f'из {total} журавликов: Сергей сделал: {Sergei}, Катя сделала: { Kate}, Петр сделал {Petr}')
+if (total % 2 == 0 and total >= 6 ) :
+    Sergei = int (total/3)//2
+    Petr =  int (total/3)//2
+    Kate = int (Sergei + Petr ) * 2
+    print (f'{total} {Sergei, Kate, Petr}')
+    
+    print (f'из {total} журавликов: Сергей сделал: {Sergei}, Катя сделала: { Kate}, Петр сделал {Petr}')
+else: 
+    (print(f'Решение невозможно') )
+

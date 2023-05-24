7b9625a457c6c3d2a4be648a411a5ee6291bac2f
Anna V <lianna2015new@yandex.ru>
2023-05-24 16:54:47 +1000

    sum

--- test-2.py
+++ test-2.py
@@ -6,7 +6,7 @@
 number = int (input('Введи трехзначное число ' ))
 valueFirst =  (number //100) 
 valueSecond = (number//10) % 10
-valueTree =  (number-100) % 10
+valueTree =  number % 10
 result = valueFirst +  valueSecond + valueTree
 print (f'Сумма цифр числа {number} = {result} ({valueFirst}+{valueSecond}+{valueTree})')
 
